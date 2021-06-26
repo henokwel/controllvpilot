4622fee1844edf68d7c55a1c37e5d96f6fb348d3
henokwel <henokwel@outlook.com>
2021-06-27 00:16:36 +0200

    init files added

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        This project to meant to help user with focus problem to stay consistent with their new habits.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
